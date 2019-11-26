b5d488a90667e188c9a248c72fec40c141eb1b82
Dawid k <dawid.kula@yahoo.co.uk>
2019-11-26 02:58:46 +0100

    Updates

---
+++ precache-manifest.1b0597c0faa90ee6893d3a610f10d2ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab0cc7582da9359ad47884771c35e11",
+    "url": "/react-redux-robots/index.html"
+  },
+  {
+    "revision": "81f8297835cfd2caca0d",
+    "url": "/react-redux-robots/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ebd53b804256bc4767e5",
+    "url": "/react-redux-robots/static/css/main.b41dcd5d.chunk.css"
+  },
+  {
+    "revision": "81f8297835cfd2caca0d",
+    "url": "/react-redux-robots/static/js/2.1b3183a6.chunk.js"
+  },
+  {
+    "revision": "ebd53b804256bc4767e5",
+    "url": "/react-redux-robots/static/js/main.9d1730e0.chunk.js"
+  },
+  {
+    "revision": "70567f1de7a45929f0ed",
+    "url": "/react-redux-robots/static/js/runtime-main.64fef0b7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-redux-robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
